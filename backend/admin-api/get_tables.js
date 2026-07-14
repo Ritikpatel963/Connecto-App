185cba8da1c3df868547077e34e2483719249543
Ritikpatel963 <rp09444467@gmail.com>
2026-07-14 17:10:05 +0530

    chore: remove unused test scripts and leftover zip/images

--- backend/admin-api/get_tables.js
+++
@@ -1,7 +0,0 @@
-const url = "https://whypwqhdfxtjjenkhkwt.supabase.co/rest/v1/?apikey=sb_publishable_3tvF2hOnQ_slfiK4dVgzVw_oSnDZpnJ";
-fetch(url)
-  .then(res => res.json())
-  .then(data => {
-     console.log(JSON.stringify(data, null, 2));
-  })
-  .catch(console.error);
